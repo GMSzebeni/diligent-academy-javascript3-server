986babe376ec523767d34cdc72dcda81f1f95b95
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2024-10-11 14:47:41 +0200

    add put and delete functions according to tasks.md

--- client2.js
+++ client2.js
@@ -18,10 +18,34 @@ const prompt = require('prompt-sync');
     console.error("Error: ", error);
 })*/
 
-fetch('http://localhost:3000/todos', {
+/* fetch('http://localhost:3000//todos', {
     method: "POST",
     headers: { "Content-Type" : "application/json" },
     body: JSON.stringify({ title: "learn JS"})
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
+.catch(error => {
+    console.error("Error: ", error);
+}) */
+
+/* fetch('http://localhost:3000/todos/3', {
+    method: "PUT",
+    headers: { "Content-Type" : "application/json" },
+    body: JSON.stringify({ title: "learn TS" })
+})
+.then(response => response.json())
+.then(data => {
+    console.log(data);
+})
+.catch(error => {
+    console.error("Error: ", error);
+}) */
+
+fetch('http://localhost:3000/todos/3', {
+    method: "DELETE"
 })
 .then(response => response.json())
 .then(data => {
--- server2.js
+++ server2.js
@@ -22,17 +22,7 @@ app.get('/todos', (req, res) => {
     res.json(todos);
 })
 
-/*app.get('/todo/:id', (req, res) => {
-    const { id } = req.params;
-    const todo = todos.find(todo => todo.id === parseInt(id));  // Fixed comparison and parsing ID
-    if (todo) {
-        res.json(todo);
-    } else {
-        res.status(404).json({ message: "Todo not found" });
-    }
-})*/
-
-/* app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', (req, res) => {
     const { id } = req.params;
 
     const todo = todos.find(todo => todo.id === parseInt(id))
@@ -42,18 +32,45 @@ app.get('/todos', (req, res) => {
     } else {
         res.status(404).json({ message: "Todo not found" });
     }
-}) */
+})
 
-/* app.post('/todos', (req, res) => {
+app.post('/todos', (req, res) => {
     const { title } = req.body;
     const id = todos.length + 1;
     const newTodo = {id: id, title: title, status: false};
     todos.push(newTodo);
     writeTodos(todos);
     res.json(todos);
-}) */
+})
 
+app.put('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
 
+    const todoIndex = todos.findIndex(todo => todo.id === parseInt(id));
+
+    if (todoIndex !== -1) {
+        todos[todoIndex] = { ...todos[todoIndex], title };
+        writeTodos(todos);
+        res.json(todos);
+    } else {
+        res.status(404).json({ message: "Todo not found" });
+    }
+})
+
+app.delete('/todos/:id', (req, res) => {
+    const { id } = req.params;
+
+    const todoIndex = todos.findIndex(todo => todo.id === parseInt(id));
+
+    if (todoIndex !== -1) {
+        todos.splice(todoIndex, 1);
+        writeTodos(todos);
+        res.json(todos);
+    } else {
+        res.status(404).json({ message: "Todo not found" });
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port: ${port}`)
